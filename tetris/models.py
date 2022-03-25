3d4b2045931e0971759d2ba2a1f3923096e0f7ab
Kraev <Mr.Krandr@mail.ru>
2022-03-25 19:22:13 +0300

    выход из ака работает

--- tetris/models.py
+++ tetris/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    # модель истории игрока
+    # тут потенциальная ошибка USER
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.CharField(max_length=50)
+    data_scope = models.DateTimeField(auto_now_add=True)
+    top_score = models.CharField(max_length=50)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
--- tetrisTest/settings.py
+++ tetrisTest/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFramepython manage.py makemigrations.Middleware',
 ]
 
 ROOT_URLCONF = 'tetrisTest.urls'
