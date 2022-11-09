3773d1867cbfbe9653017685a2bc3c243bc00328
Kraev <Mr.Krandr@mail.ru>
2022-11-09 21:35:54 +0300

    FINAL

--- tetrisTest/wsgi.py
+++ tetrisTest/wsgi.py
@@ -1,20 +1,20 @@
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
 
-import tetris.routing
+# import tetris.routing
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tetrisTest.settings')
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-    URLRouter(
-      tetris.routing.websocket_urlpatterns
-    )
-  )
-})
-# application = get_wsgi_application()
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#     URLRouter(
+#       tetris.routing.websocket_urlpatterns
+#     )
+#   )
+# })
+application = get_wsgi_application()
